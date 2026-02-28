d256be46dfdf526327c39aa7abdf444c82295ab0
Dev Joseph <joembalmes06@gmail.com>
2026-02-28 10:54:45 +0800

    [add] web title

--- index.php
+++ index.php
@@ -9,13 +9,11 @@ include("phpscripts/check-login.php");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Phinma UCL Library</title>
   <?php include("components/head.php"); ?>
 </head>
 
 <body>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut dolor, qui repellat architecto in ipsa
-    fuga illo ullam odit pariatur enim eius hic commodi magnam similique, nulla labore necessitatibus.</p>
 
   <?php include("components/scripts.php"); ?>
 </body>
