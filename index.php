bc1219238bd348d11e0ad7c95b5902d6337e3d74
Dev Joseph <joembalmes06@gmail.com>
2026-02-28 10:41:58 +0800

    [add] bootstrap 5

--- index.php
+++ index.php
@@ -10,11 +10,14 @@ include("phpscripts/check-login.php");
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <?php include("components/head.php"); ?>
 </head>
 
 <body>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aut dolor, qui repellat architecto in ipsa
     fuga illo ullam odit pariatur enim eius hic commodi magnam similique, nulla labore necessitatibus.</p>
+
+  <?php include("components/scripts.php"); ?>
 </body>
 
 </html>
