42dfe35c38098c2f878f5e1d6aec4155eff0af14
Dev Joseph <joembalmes06@gmail.com>
2026-04-27 09:07:17 +0800

    Update database-connection.php

--- phpscripts/database-connection.php
+++ phpscripts/database-connection.php
@@ -4,6 +4,11 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "library_db";
 
+// $dbhost = "localhost";
+// $dbuser = "think2025_user";
+// $dbpass = "&BUiocsWJBGLjt)2";
+// $dbname = "think2025_thinktech_library";
+
 if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
   die("failed to connect!");
 }
